003879478ede020d9cd001b84dbcbd33ac77f48c
goktuggokcen <goktuggokcen0@gmail.com>
2024-03-11 15:34:35 +0200

    Fix file selection and page merging logic

--- PDF-Merger/main.py
+++ PDF-Merger/main.py
@@ -58,9 +58,9 @@ class App(customtkinter.CTk):
     # Merge the files.
     def merge_files(self):
         # Check if there are at least two files selected.
-        if len(self.file_element.selected_files) < 2:
+        if len(self.file_element.selected_files) == 0:
             messagebox.showerror(
-                "Error", "Select at least two files to merge.", parent=self
+                "Error", "Select at least one files to merge.", parent=self
             )
 
         # Merge the files.
@@ -72,10 +72,14 @@ class App(customtkinter.CTk):
             for selected_file in self.file_element.selected_files:
                 # Open each file and add its pages to the writer object.
                 with open(selected_file.file_path, "rb") as file:
-                    pdf_reader = PyPDF2.PdfReader(file)
-                    for page_num in range(len(pdf_reader.pages)):
-                        page = pdf_reader.pages[page_num]
-                        pdf_writer.add_page(page)
+                    if selected_file.is_page_selected:
+                        for page_num in selected_file.selected_pages:
+                            page = selected_file.pdf_reader.pages[page_num]
+                            pdf_writer.add_page(page)
+                    else:
+                        for page_num in range(len(selected_file.pdf_reader.pages)):
+                            page = selected_file.pdf_reader.pages[page_num]
+                            pdf_writer.add_page(page)
 
             # Save the merged PDF to a file.
             output_file_path = filedialog.asksaveasfilename(
@@ -124,6 +128,9 @@ class Selected_File(customtkinter.CTkFrame):
         super().__init__(master=master)
         self.pack(side="top", fill="x", padx=5, pady=5)
         self.file_path = file_path
+        self.is_page_selected = False
+        self.selected_pages = []
+        self.pdf_reader = PyPDF2.PdfReader(file_path)
         self.create_widgets()
 
     # Create the widgets.
@@ -138,6 +145,12 @@ class Selected_File(customtkinter.CTkFrame):
             hover_color="#c0392b",
         )
         self.remove_button.pack(side="right", padx=5, pady=5)
+
+        # Select the page number.
+        self.select_page_number_button = customtkinter.CTkButton(
+            master=self, text="Edit", command=self.select_page_number, width=60
+        )
+        self.select_page_number_button.pack(side="right", padx=5, pady=5)
 
         # Create the label.
         self.label = customtkinter.CTkLabel(master=self, text=self.file_path)
@@ -154,6 +167,47 @@ class Selected_File(customtkinter.CTkFrame):
                 master=self.master, text="No file selected."
             )
             self.master.label.pack(side="top", fill="x", padx=5, pady=5)
+
+    # Select the page number.
+    def select_page_number(self):
+        self.select_page_window = customtkinter.CTkToplevel(self.master.master)
+        self.select_page_window.title("Select Page Number")
+        self.select_page_window.geometry("300x300")
+
+        # Create the select page number button.
+        select_button = customtkinter.CTkButton(
+            self.select_page_window,
+            text="Selects",
+            command=lambda: self.get_selected_pages(checkboxes),
+        )
+        select_button.pack(side="bottom", fill="x", padx=5, pady=5)
+
+        # Create the scrollable frame.
+        pages = customtkinter.CTkScrollableFrame(self.select_page_window)
+        pages.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+
+        # Iterate over the pages.
+        checkboxes = []
+        for page in range(len(self.pdf_reader.pages)):
+            # Create the checkbox.
+            checkbox = customtkinter.CTkCheckBox(
+                master=pages, text="Page {}".format(page + 1)
+            )
+            checkbox.pack(side="top", fill="x", padx=5, pady=5)
+            checkboxes.append(checkbox)
+
+    # Get the selected pages.
+    def get_selected_pages(self, checkboxes: list) -> None:
+        if len(checkboxes) == 0:
+            self.select_page_window.destroy()
+            return
+
+        self.selected_pages = []
+        for checkbox in checkboxes:
+            if checkbox.get():
+                self.selected_pages.append(int(checkbox._text.split()[1]) - 1)
+                self.is_page_selected = True
+        self.select_page_window.destroy()
 
 
 # Check the file run directly or as a module.
